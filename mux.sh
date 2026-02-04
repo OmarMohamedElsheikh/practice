57e65bdea7d888f0e7a134dfb214f2e60d6f910c
OmarMohamedElsheikh <omarmohammedomarali@gmail.com>
2026-02-04 18:37:06 +0200

    adding verbose , dry-run flags

--- mux.sh
+++ mux.sh
@@ -4,6 +4,20 @@ shopt -s nullglob
 
 dir="${1:?Usage: $0 <dir>}"
 
+PARSED=$(getopt --options "" --long verbose,dry-run -- "$@") || exit 1
+eval set -- "$PARSED"
+
+verbose=0
+dry-run=0
+while true; do 
+	case "$1" in 
+		--verbose) verbose=1; shift ;;
+		--dry-run) dry-run=1; shift ;;
+		--) shift; break ;;
+		*) echo "Usage: $0 [--verbose] [--dry-run] <dir>"
+	esac
+done
+
 find "$dir" -type f -name "*.mp4" -print0 | while IFS= read -d -r '' file; do
 
 	base="${file%.mp4}"
