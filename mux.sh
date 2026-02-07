408bbb1b215cdcef6f46de8aa2f54023aada835c
OmarMohamedElsheikh <omarmohammedomarali@gmail.com>
2026-02-07 18:10:03 +0200

    fixing dryrun flag

--- mux.sh
+++ mux.sh
@@ -3,21 +3,21 @@
 shopt -s nullglob
 set -euo pipefail
 
-dir="${1:?Usage: $0 [--verbose] [--dry-run] <dir>}"
+dir="${1:?Usage: $0 [--verbose] [--dryrun] <dir>}"
 
 
-PARSED=$(getopt --options "" --long verbose,dry-run -- "$@") || exit 1
+PARSED=$(getopt --options "" --long verbose,dryrun -- "$@") || exit 1
 eval set -- "$PARSED"
 
 verbose=0
-dry-run=0
+dryrun=0
 
 while true; do 
 	case "$1" in 
 		--verbose) verbose=1; shift ;;
-		--dry-run) dry-run=1; shift ;;
+		--dryrun) dryrun=1; shift ;;
 		--) shift; break ;;
-		*) echo "Usage: $0 [--verbose] [--dry-run] <dir>"
+		*) echo "Usage: $0 [--verbose] [--dryrun] <dir>"
 	esac
 done
 
@@ -41,7 +41,7 @@ find "$dir" -type f -name "*.mp4" -print0 | while IFS= read -d -r '' file; do
 	output="${base}.mux.mp4"
 	[[ -f "$output" ]] && echo "Skipping $output file exist" && continue
 
-	[[ $dry-run -eq 1 ]] && echo "ffmpeg -i $file -i $audio $output && rm -- $file $audio" && continue
+	[[ $dryrun -eq 1 ]] && echo "ffmpeg -i $file -i $audio $output && rm -- $file $audio" && continue
 	
 	if ffmpeg -loglevel error -i "$file" -i "$audio" -c copy "$output" 2>&1; then
 	        if [[ -s "$output" ]]; then
